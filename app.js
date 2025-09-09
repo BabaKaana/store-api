647f9d84d44a0ae6b9197c3fa94592d490f1c235
BabaKaana <samiulhaq744@gmail.com>
2025-09-09 16:10:19 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+
+const express = require('express');
+const app = express();
+
+
+const notFound = require('./middleware/not-found');
+const errorHandler = require('./middleware/error-handler');
+
+app.use(express.json());
+
+
+app.get('/', (req,res)=>{
+    res.send('<h1>Store API</h1><a href="/api/v1/products">Products Link</a>')
+})
+
+app.use(notFound);
+app.use(errorHandler);
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        app.listen(port, console.log(`Server is listening on port ${port}`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
