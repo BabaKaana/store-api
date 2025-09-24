8e508b450cf8c13f432abc8c9ab09d93fdfe62d7
babakaana <samiulhaq744@gmail.com>
2025-09-24 23:00:13 +0500

    added products filtering

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,37 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
-const getAllProductsStatic = async (req,res) => {
-    const products = await Product.find({featured: true});
-    res.status(200).json({ products , nbHits: products.length})
-}
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({}).sort("-price");
+  res.status(200).json({ nbHits: products.length, products });
+};
 
-const getAllProducts = async (req,res) => {
-    const products = await Product.find(req.query);
-    res.status(200).json({ products , nbHits: products.length})
-}
+const getAllProducts = async (req, res) => {
+  const { featured, company, name, sort } = req.query;
+  const queryObject = {};
 
+  if (featured) {
+    queryObject.featured = featured;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
 
-module.exports = {getAllProducts, getAllProductsStatic}
+  // console.log(queryObject);
+
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+
+  const products = await result;
+
+  res.status(200).json({ nbHits: products.length, products });
+};
+
+module.exports = { getAllProducts, getAllProductsStatic };
